0624a2ff218760ac2cfd879e5972fe6d48590bf2
Shannon Simpson <Shannon.Simpson1020@gmail.com>
2022-12-19 18:43:24 +0000

    closing h3 tags

--- 01_01b/index.php
+++ 01_01b/index.php
@@ -12,9 +12,9 @@
 		if (isset($_POST['submit'])) {
 			$num = (int)$_POST('number');
 			if (is_int($num) && $num > 0) {
-				echo '<h3>Great! You have submitted a postitive integer!';
+				echo '<h3>Great! You have submitted a postitive integer!</h3>';
 			} else {
-				echo '<h3>ERROR: Dude, you need to submit a postitive integer!';
+				echo '<h3>ERROR: Dude, you need to submit a postitive integer!</h3>';
 			}
 		}
 		?>
